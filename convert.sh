e8eab1f2291cd39fc28cf945085a71c24ba11884
chrisbenti <chrisbenti@users.noreply.github.com>
2024-08-09 23:23:21 +0000

    deploy: 08fa0193a1f8d8c88f17f61c460dbde919c972be

--- convert.sh
+++ convert.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 rm favicon.ico
-rm qr*png
 convert -density 256x256 -background transparent favicon.svg -define icon:auto-resize -colors 256 favicon.ico
-convert -density 2048x2048 -background transparent qr.svg qr-transparent.png
-convert -density 2048x2048 -background '#ffffff' -alpha remove qr.svg qr-white.png
+
