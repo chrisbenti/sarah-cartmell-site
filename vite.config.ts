a60939daa60b30205a2998bc4bd8e4719a8d53cf
Chris Bentivenga <chris@chrisbenti.com>
2024-08-09 14:15:25 -0700

    remove base url

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-    base: '/sarah-cartmell-site/', // Remove me when Sarah gets her own domain
     plugins: [
         react({
             jsxImportSource: '@emotion/react',
