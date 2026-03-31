bdf21aee1f85648a4dd4384ad1e77ab68697cba9
Chris Bentivenga <chris@chrisbenti.com>
2026-03-31 15:25:09 -0400

    knip

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ export const SMALL_SCREEN_SIZE = '576px'
 
 // Colors (Academic/Minimalist Theme)
 export const PRIMARY_COLOR = '#4682B4' // Steel Blue
-export const SECONDARY_COLOR = '#6A5ACD' // Slate Blue (subtle accent)
+const SECONDARY_COLOR = '#6A5ACD' // Slate Blue (subtle accent)
 export const TEXT_COLOR = '#333333' // Dark Grey for text
 export const BACKGROUND_COLOR = '#F5F5F5' // Light Grey background
 export const ACCENT_COLOR = '#D3D3D3' // Light Grey for borders/subtle elements
@@ -11,4 +11,4 @@ export const ACCENT_COLOR = '#D3D3D3' // Light Grey for borders/subtle elements
 export const FONT_SIZE_XL = '3.5em' // Slightly smaller for academic feel
 export const FONT_SIZE_LG = '2em'
 export const FONT_SIZE_MD = '1.1em'
-export const FONT_SIZE_SM = '0.85em'
+const FONT_SIZE_SM = '0.85em'
